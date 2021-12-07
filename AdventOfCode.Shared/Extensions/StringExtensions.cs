922ca6873130aef9180916e2bdf1e69918f97ec3
Martijn Boshoven <martijn.boshoven@rhdhv.com>
2021-12-07 07:27:22 +0100

    Added csv extension

---
+++ AdventOfCode.Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode.Shared.Extensions;
+
+public static class StringExtensions
+{
+    public static IEnumerable<T> AsCsv<T>(this string line, char seperator = ',', StringSplitOptions options = StringSplitOptions.None) =>
+        line.Split(seperator, options).Select(x => (T)Convert.ChangeType(x, typeof(T))).ToList();
+}
