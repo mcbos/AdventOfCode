0f8ae7cc59838230da64a3f07c767c0a7b632623
Martijn <mboshoven@gmail.com>
2021-12-08 06:04:13 +0100

    Day 7 completed (#2)
    * Day 7 preparations
    
    * Added csv extension
    
    * Completed day 7
    
    * Implemented csv extension usage in other days

---
+++ AdventOfCode.2021/Challenges/Day07.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode._2021.Challenges;
+
+[Day(2021, 7)]
+public class Day07 : Day<IList<int>>
+{
+    public override IList<int> ParseInput(string[] lines) => (IList<int>)lines.First().AsCsv<int>();
+
+    public override object ExecutePart1() =>
+        Enumerable.Range(0, Input.Count).Min(i => Input.Sum(x => Math.Abs(i - x)));
+
+    public override object ExecutePart2() =>
+        Enumerable.Range(0, Input.Count).Min(i => Input.Sum(x => (Math.Abs(i - x) * (Math.Abs(i - x) + 1)) / 2));
+}
